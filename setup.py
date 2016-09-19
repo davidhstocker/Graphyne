10c1fc77bf2ce8ba5b0a87a60464212771e97c31
David Stocker <david.stocker@sap.com>
2016-09-19 16:17:11 -0700

    Added setup.py and setup.cfg

---
+++ setup.py
@@ -0,0 +1,19 @@
+'''
+Created on September 19, 2016
+
+@author: David Stocker
+'''
+
+from distutils.core import setup
+setup(
+  name = 'graphyne',
+  packages = ['graphyne'],
+  version = '1.0',
+  description = "A fully native Python property graph database. It is designed to be easy to integrate into your Python projects, simple to use and very powerful.",
+  author = 'David Stocker',
+  author_email = 'mrdave991@gmail.com',
+  url = 'https://github.com/davidhstocker/Graphyne',
+  download_url = 'https://github.com/davidhstocker/Graphyne/tarball/1.0', 
+  keywords = ['graph', 'propertygraph', 'property graph', 'graphdatabase', 'graph database'], # arbitrary keywords
+  classifiers = [],
+)
