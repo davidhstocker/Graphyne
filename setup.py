0d8b01e0c55cf87e679f696e66f1feeb7a4a3f0d
David Stocker <david.stocker@sap.com>
2017-01-27 22:47:38 +0100

    XML Cleanup

--- setup.py
+++ setup.py
@@ -8,15 +8,13 @@ from distutils.core import setup
 setup(
   name = 'graphyne',
   packages = ['graphyne'],
-  version = '1.1',
-  description = "A fully native Python property graph database. It is designed to be easy to integrate into your Python projects, simple to use and very powerful.",
+  version = '1.2',
+  description = "A fully native Python 'smart graph' property graph database. It is designed to be easy to integrate into your Python projects, simple to use and very powerful.",
   author = 'David Stocker',
   author_email = 'mrdave991@gmail.com',
   url = 'https://github.com/davidhstocker/Graphyne',
-  download_url = 'https://github.com/davidhstocker/Graphyne/tarball/1.1', 
-  keywords = ['graph', 'propertygraph', 'property graph', 'graphdatabase', 'graph database'],
-  install_requires=[
-          'memetic',
-      ],
+  download_url = 'https://github.com/davidhstocker/Graphyne/tarball/1.2', 
+  keywords = ['graph', 'propertygraph', 'property graph', 'graphdatabase', 'graph database', 'smart graph', 'smartgraph'],
+  install_requires=[],
   classifiers = [],
 )
